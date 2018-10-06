d84871f8983ae4b2f33e132f4d70ac8b93a0592a
DillonSimeone <Dillonsimeone@gmail.com>
2018-10-05 23:10:18 -0700

    Big clean-up
    Todo: Indenting.
    
    I'm getting this out there ASAP, since a lot have changed.
    
    Fixing indenting now.

--- src/scripts/home/home.js
+++ src/scripts/home/home.js
@@ -1,5 +1,4 @@
 // function display the next image from the array
-
 function displayNextImage() {
     x = (x === images.length - 1) ? 0 : x + 1;
     document.getElementById("img").src = images[x];
@@ -13,22 +12,29 @@ function goToHalloween(){
   window.location.href = 'haloween.html';
 }
 
-function displayPreviousImage() {
-    x = (x <= 0) ? images.length - 1 : x - 1;
-    document.getElementById("img").src = images[x];
-}
-
-function startTimer() {
-    setInterval(displayNextImage, 1000);
-}
-
-var images = [], x = -1;
-images[0] = "images/home/image1.jpg";
-images[1] = "images/home/image2.jpg";
-images[2] = "images/home/image3.jpg";
-
 function animatedScroll(elem) {
     $('html, body').animate({
         scrollTop: $(elem).offset().top
     }, 800);
 }
+
+function displayPreviousImage() {
+    x = (x <= 0) ? images.length - 1 : x - 1;
+    document.getElementById("img").src = images[x];
+}
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("startPicture").style.backgroundImage = "url(./images/home/" + images[x] + ")";
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 3000);
+}
+
+var images = [], x = -1;
+images[0] = "image1.jpg";
+images[1] = "image2.jpg";
+images[2] = "image3.jpg";
+
+
