dac4698eff75205fc0ef9afd7ee8b441e88f2be2
masihtamsoy <masihtamsoy@gmail.com>
2018-10-24 18:51:19 +0530

    Added halloween.js and moved all halloween.html related functions

--- src/scripts/halloween/halloween.js
+++ src/scripts/halloween/halloween.js
@@ -1,4 +1,27 @@
 /*
+   Displaying the time left for Halloween
+*/
+
+var waitdate=new Date("Oct 31, 2018 00:00:00").getTime();
+
+//function to update the timer every second
+
+var x=setInterval(function(){
+       var rightnow=new Date().getTime();
+       var distance=waitdate-rightnow;
+       var days=Math.floor(distance/(1000*60*60*24));
+       var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+       var min=Math.floor((distance%(1000*60*60))/(1000*60));
+       var sec=Math.floor((distance%(1000*60))/1000);
+       document.getElementById("countdown").innerHTML=days+"d "+hours+"h "+min+"m "+sec+"s";
+       if(distance<0){
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML="Happy Halloween!";
+        }
+},1000);
+
+
+/*
  * Konami code
  *
  */
@@ -43,6 +66,10 @@ function executeEasterEgg() {
   document.documentElement.scrollTop = 0;
 }
 
+
+/**
+ * Witch riding broomstick
+ */
 function myMove() {
   var elem = document.getElementById("floatingwitch");
   var elem1 = document.getElementById("jumbotron");
