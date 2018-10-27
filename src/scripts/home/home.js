be7559239425a269d1a834f1b4fd218803955a83
Arpita <ganatraarpita@gmail.com>
2018-10-27 15:54:28 +0530

    Added a popup

--- src/scripts/home/home.js
+++ src/scripts/home/home.js
@@ -7,3 +7,15 @@ function animatedScroll(elem) {
         scrollTop: $(elem).offset().top
     }, 800);
 }
+ $(document).ready(function () {
+
+    //select the POPUP FRAME and show it
+    $("#popup").hide().fadeIn(1000);
+
+    //close the POPUP if the button with id="close" is clicked
+    $("#close").on("click", function (e) {
+        e.preventDefault();
+        $("#popup").fadeOut(1000);
+    });
+
+})
