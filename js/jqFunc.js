6c49cb80bdc4bb64dd02440b3c3766df50645949
edizuslu <36105513+edizuslu@users.noreply.github.com>
2018-10-29 19:49:07 +0300

    Update js/jqFunc.js

---
+++ js/jqFunc.js
@@ -0,0 +1,25 @@
+// The following variables are defined in the global scope
+var num1 = 20,
+    num2 = 3,
+    name = 'Chamahk';
+
+// This function is defined in the global scope
+function multiply() {
+  return num1 * num2;
+}
+
+multiply(); // Returns 60
+
+// A nested function example
+function getScore() {
+  var num1 = 2,
+      num2 = 3;
+  
+  function add() {
+    return name + ' scored ' + (num1 + num2);
+  }
+  
+  return add();
+}
+
+getScore(); // Returns "Chamahk scored 5"
